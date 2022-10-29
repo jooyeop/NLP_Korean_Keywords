08c575e487fbcf777fb785e24e9c3bbb277aa85a
jooyeop <royalmansu@gmail.com>
2022-10-30 07:04:11 +0900

    NLP

--- keywords.py
+++ keywords.py
@@ -9,23 +9,119 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer
 
-doc = """
-드론 활용 범위도 점차 확대되고 있다. 최근에는 미세먼지 관리에 드론이 활용되고 있다.
-서울시는 '미세먼지 계절관리제' 기간인 지난달부터 오는 3월까지 4개월간 드론에 측정장치를 달아 미세먼지 집중 관리를 실시하고 있다.
-드론은 산업단지와 사업장 밀집지역을 날아다니며 미세먼지 배출 수치를 점검하고, 현장 모습을 영상으로 담는다.
-영상을 통해 미세먼지 방지 시설을 제대로 가동하지 않는 업체와 무허가 시설에 대한 단속이 한층 수월해질 전망이다.
-드론 활용에 가장 적극적인 소방청은 광범위하고 복합적인 재난 대응 차원에서 드론과 관련 전문인력 보강을 꾸준히 이어가고 있다.
-지난해 말 기준 소방청이 보유한 드론은 총 304대, 드론 조종 자격증을 갖춘 소방대원의 경우 1,860명이다.
-이 중 실기평가지도 자격증까지 갖춘 ‘드론 전문가’ 21명도 배치돼 있다.
-소방청 관계자는 "소방드론은 재난현장에서 영상정보를 수집, 산악ㆍ수난 사고 시 인명수색·구조활동,
-유독가스·폭발사고 시 대원안전 확보 등에 활용된다"며
-"향후 화재진압, 인명구조 등에도 드론을 활용하기 위해 연구개발(R&D)을 하고 있다"고 말했다.
+doc1 = """
+최근 LED(Light-Emitting Diode) TV 시장의 급성장에 따라 LED BLU(Back Light Unit)를 제어 하는 IC 또한 중요성이 커지고 있으며,
+DC-DC 컨버터는 효율이 높은 LLC 공진컨버터로 대체되고, 단가를 낮추기 위헤 PCB 에 실장 되는 개별 소자들로 이루어진 블록을 하나의 IC로 집적하는 연구가 활발하게 진행되고 있다.
 """
 
 okt = Okt()
 
 # doc 핵심 단어
-tokenized_doc = okt.pos(doc)
+tokenized_doc = okt.pos(doc1)
 
 tokenized_nouns = ' '.join([word[0] for word in tokenized_doc if word[1] == 'Noun'])
-print('핵심 키워드 :',tokenized_nouns)
+
+
+# 품사추출
+def get_noun(text):
+    tokenized_doc = okt.pos(text)
+    tokenized_nouns = ' '.join([word[0] for word in tokenized_doc if word[1] == 'Noun'])
+    return tokenized_nouns
+print(get_noun(doc1))
+
+# 문장 추출
+def get_sentence(text):
+    tokenized_doc = okt.pos(text)
+    tokenized_sentence = ' '.join([word[0] for word in tokenized_doc if word[1] == 'Punctuation'])
+    return tokenized_sentence
+
+print(get_sentence(doc1))
+
+# 중요 단어
+def get_important_word(text):
+    tokenized_doc = okt.pos(text)
+    tokenized_important_word = ' '.join([word[0] for word in tokenized_doc if word[1] == 'Verb'])
+    return tokenized_important_word
+print(get_important_word(doc1))
+
+# 명사 추출
+def get_nouns(text):
+    tokenized_doc = okt.pos(text)
+    tokenized_nouns = ' '.join([word[0] for word in tokenized_doc if word[1] == 'Noun'])
+    return tokenized_nouns
+print(get_nouns(doc1))
+
+# tensorflow 모델 학습
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+
+# 데이터셋
+batch_size = 32
+seed = 42
+
+raw_train_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    'aclImdb/train',
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='training',
+    seed=seed)
+
+for text_batch, label_batch in raw_train_ds.take(1):
+    for i in range(3):
+        print("Review", text_batch.numpy()[i])
+        print("Label", label_batch.numpy()[i])
+
+# 데이터셋 전처리
+max_features = 10000
+sequence_length = 250
+
+vectorize_layer = TextVectorization(
+    max_tokens=max_features,
+    output_mode='int',
+    output_sequence_length=sequence_length)
+
+
+# 데이터셋 만들기
+train_text = raw_train_ds.map(lambda x, y: x)
+vectorize_layer.adapt(train_text)
+
+# 모델 만들기
+embedding_dim = 16
+
+model = tf.keras.Sequential([
+    layers.Embedding(max_features + 1, embedding_dim),
+    layers.Dropout(0.2),
+    layers.GlobalAveragePooling1D(),
+    layers.Dropout(0.2),
+    layers.Dense(1)])
+
+model.summary()
+
+# 모델 컴파일
+model.compile(loss=keras.losses.BinaryCrossentropy(from_logits=True),
+                optimizer='adam',
+                metrics=tf.metrics.BinaryAccuracy(threshold=0.0))
+
+# 모델 훈련
+epochs = 10
+history = model.fit(
+    raw_train_ds,
+    validation_data=doc1,
+    epochs=epochs)
+
+
+# 모델 평가
+loss, accuracy = model.evaluate(doc1)
+
+print("Loss: ", loss)
+print("Accuracy: {:2.2%}".format(accuracy))
+
+# 모델 예측
+export_model = tf.keras.Sequential([
+    vectorize_layer,
+    model,
+    layers.Activation('sigmoid')])
+export_model.compile(
+    loss=keras.losses.BinaryCrossentropy(from_logits=False), optimizer="adam", metrics=['accuracy'])
